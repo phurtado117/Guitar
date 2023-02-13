713f60420311b28f0f3c24eb5912f0441bb570d3
Pablo Hurtado <phurtado127@gmail.com>
2023-02-13 12:32:51 -0600

    se crea clase guitarra

---
+++ src/GuiatarraPablo.java
@@ -0,0 +1,10 @@
+public class GuiatarraPablo {
+    private String serialNumber;
+    public String getSerialNumber(){
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}
---
+++ src/guitar.java
@@ -0,0 +1,10 @@
+public class guitar {
+    private String serialNumber;
+    private double price;
+
+    private String builder;
+    private String model;
+    private String type;
+    private String backWood;
+    private String topWood;
+}
